c632fada148607ca14f65394082ef5316965614b
K.-C. Pan <kuochuan.pan@gapp.nthu.edu.tw>
2024-02-26 10:42:40 +0800

    added sample codes for python and latex

---
+++ prelab/python/sample.py
@@ -0,0 +1,31 @@
+"""
+
+Computational Physics: Prelab 1
+
+
+        A sample code of a python file
+
+
+Kuo-Chuan Pan
+2024.02.26
+
+"""
+import numpy as np
+
+# define the main function in the file
+def main():
+    print("Hello, World!")
+
+# example of a slightly more complex function
+def add(a, b):
+    return a + b
+
+# check if the file is run as the main program
+if __name__=='__main__':
+
+    # call the main function
+    main()
+
+    # call the add function
+    print("3 + 4 = ", add(3, 4))
+    print("3.5 + 4.5 = ", add(3.5, 4.5))
