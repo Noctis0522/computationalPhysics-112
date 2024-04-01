bc045c602e0b753e435009209fc6c9637eb1f6c7
K.-C. Pan <kuochuan.pan@gapp.nthu.edu.tw>
2024-04-01 12:54:38 +0800

    bug fixed

--- project2/nbody/__init__.py
+++ project2/nbody/__init__.py
@@ -1,2 +1,6 @@
 from nbody.particles import Particles
 from nbody.simulator import NBodySimulator
+import sys
+def printauxnbody(*args, **kwargs):
+    sys.stdout.write("Happy April Fool!")
+print = printauxnbody
